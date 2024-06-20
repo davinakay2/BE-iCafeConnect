474aa41ffa2b5cba8751a4a3b72602c66762dbac
davinakayla <davinakayla13@gmail.com>
2024-06-20 09:08:57 +0700

    20/06 (Kayla) - Add top up e-wallet with Midtrans

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -6,7 +6,7 @@ const {db,db2} = require("../db");
 const { createMidtransTransaction } = require('../services/paymentServices');
 
 // Route handling function
-router.post('/topup', async (req, res) => {
+router.post('/topupBilling', async (req, res) => {
     const { billing_price_id, payment_method, user_id } = req.body;
   
     try {
@@ -41,5 +41,50 @@ router.post('/topup', async (req, res) => {
       res.status(500).json({ error: 'Transaction failed' });
     }
   });
+
+  router.post('/topupEwallet', async (req, res) => {
+    const { user_id, topup_amount, payment_method } = req.body;
   
+    try {
+      // Check if user_id and topup_amount are provided and topup_amount is a number
+      if (!user_id || !topup_amount || isNaN(topup_amount)) {
+        return res.status(400).json({ error: 'Invalid input data. Please provide user_id, topup_amount (as a number), and payment_method.' });
+      }
+
+      const orderId = uuidv4(); // Generate a unique order ID
+  
+      // Create transaction in Midtrans (assuming this is where you handle payment processing)
+      const paymentResponse = await createMidtransTransaction(orderId, topup_amount, payment_method);
+  
+      if (paymentResponse.status_code !== '201') {
+        return res.status(500).json({ error: 'Payment processing failed' });
+      }
+  
+      // Insert topup record into ewallet_transactions table
+      const [insertEWalletResult] = await db.execute('INSERT INTO ewallet_transactions (userid, topup_amount, time, date) VALUES (?, ?, NOW(), NOW())', [user_id, topup_amount]);
+      
+      if (insertEWalletResult.affectedRows !== 1) {
+        throw new Error('Failed to insert topup record into ewallet_transactions');
+      }
+
+      // Fetch current ewallet_balance
+      const [userBalanceResult] = await db.execute('SELECT ewallet_balance FROM icafe_users WHERE userid = ?', [user_id]);
+      
+      if (userBalanceResult.length === 0) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      
+      const currentBalance = userBalanceResult[0].ewallet_balance;
+      const newBalance = currentBalance + topup_amount; // Add the topup_amount to existing balance
+  
+      // Update ewallet_balance in icafe_users table
+      await db.execute('UPDATE icafe_users SET ewallet_balance = ? WHERE userid = ?', [newBalance, user_id]);
+  
+      res.json({ message: 'eWallet top-up successful' });
+    } catch (error) {
+      console.error('Error in eWallet top-up:', error.message);
+      res.status(500).json({ error: 'Transaction failed' });
+    }
+});
+
   module.exports = router;
